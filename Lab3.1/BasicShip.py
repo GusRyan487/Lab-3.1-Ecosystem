3cdd0f15b4cfdf7f61d8400d4c411c95b11b7f11
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-08 11:05:10 -0600

    Update BasicShip.py

--- Lab3.1/BasicShip.py
+++ Lab3.1/BasicShip.py
@@ -1,12 +1,29 @@
-from updatedGraphics import *
+from graphics import *
 win = GraphWin('Navy',1000,650,autoflush= False)
 class basicShip(object):
 
-    def __init__(self,x,y,color = (color_rgb(150, 155, 163))):
-        self.X = x
-        self.Y = y
+    def __init__(self,x,y,win,color = (color_rgb(150, 155, 163))):
+        self.x = x
+        self.y = y
         self.color = color
         self.hit = False
+        win = win
+        self.hull()
+        self.controlCenter()
 
-    def draw(self):
-        hull = Polygon()
+    def hull(self):
+        hull = Polygon(Point(self.x,self.y),Point((self.x + 75), (self.y + 50)), Point((self.x + 225 ), (self.y + 50)), Point((self.x + 300), (self.y)))
+        hull.setFill(self.color)
+        hull.draw(win)
+
+    def controlCenter(self):
+        controlCenter = Polygon(Point((self.x + 150 ), (self.y - 0)),Point((self.x + 175 ), (self.y - 75)),Point((self.x + 250 ), (self.y - 75)),Point((self.x + 250 ), (self.y - 0)))
+        controlCenter.setFill(self.color)
+        controlCenter.draw(win)
+
+
+
+
+ship = basicShip(200,300,win)
+win.getMouse()
+
