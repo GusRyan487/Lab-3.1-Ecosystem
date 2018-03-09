657f09dae5cf8e37561db477d728b3ce86d6ca1b
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-09 10:56:16 -0600

    Update BasicShip.py

--- Lab3.1/BasicShip.py
+++ Lab3.1/BasicShip.py
@@ -1,5 +1,5 @@
 from graphics import *
-win = GraphWin('Navy',1000,650,autoflush= False)
+#win = GraphWin('Navy',1000,650,autoflush= False)
 class basicShip(object):
 
     def __init__(self,x,y,win,color = (color_rgb(150, 155, 163))):
@@ -7,23 +7,28 @@ class basicShip(object):
         self.y = y
         self.color = color
         self.hit = False
-        win = win
+        self.win = win
         self.hull()
         self.controlCenter()
+        self.windows()
 
     def hull(self):
         hull = Polygon(Point(self.x,self.y),Point((self.x + 75), (self.y + 50)), Point((self.x + 225 ), (self.y + 50)), Point((self.x + 300), (self.y)))
         hull.setFill(self.color)
-        hull.draw(win)
+        hull.draw(self.win)
 
     def controlCenter(self):
         controlCenter = Polygon(Point((self.x + 150 ), (self.y - 0)),Point((self.x + 175 ), (self.y - 75)),Point((self.x + 250 ), (self.y - 75)),Point((self.x + 250 ), (self.y - 0)))
         controlCenter.setFill(self.color)
-        controlCenter.draw(win)
+        controlCenter.draw(self.win)
 
+    def windows(self):
+        addX = 165.5
+        for i in range(0,4):
+            window = Rectangle(Point((self.x + addX),(self.y - 50)), Point((self.x + (addX + 12.5)),(self.y - 37.5)))
+            window.setFill(color_rgb(198,244,242))
+            window.draw(self.win)
+            addX += 15
 
-
-
-ship = basicShip(200,300,win)
-win.getMouse()
-
+#ship = basicShip(600,350,win)
+#win.getMouse()
