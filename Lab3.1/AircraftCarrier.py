e48baa91416b0e32f2d616a1b46cc8f9dd8e7e4a
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-11 15:41:17 -0500

    Update AircraftCarrier.py

--- Lab3.1/AircraftCarrier.py
+++ Lab3.1/AircraftCarrier.py
@@ -1,1 +1,7 @@
-'AC'
+from BasicShip import *
+class AircraftCarrier(basicShip):
+    def __init__(self, x, y, win, color=(color_rgb(150, 155, 163))):
+        super(AircraftCarrier,self).__init__(x,y,win,color)
+
+    def plane(self):
+
