75c275e5047f004c623a16575401c9fd0c0708ff
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-11 15:42:34 -0500

    Update Destroyer.py

--- Lab3.1/Destroyer.py
+++ Lab3.1/Destroyer.py
@@ -1,1 +1,7 @@
-'D'
+from BasicShip import *
+class Destroyer(basicShip):
+    def __init__(self, x, y, win, color=(color_rgb(150, 155, 163))):
+        super(Destroyerr,self).__init__(x,y,win,color)
+
+    def turret(self):
+
