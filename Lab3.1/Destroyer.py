b8879cf16228f3f0f71d4abc262fe2df4c51e8da
Skidaddle <36928558+MrGoose100@users.noreply.github.com>
2018-03-12 09:26:08 -0500

    Update Destroyer.py

--- Lab3.1/Destroyer.py
+++ Lab3.1/Destroyer.py
@@ -5,3 +5,12 @@ class Destroyer(basicShip):
 
     def turret(self):
 
+from basicShip import *
+class Destroyer(basicShip):
+    def __init__(self, x, y, win, color=(color_rgb(150, 155, 163))):
+        super(Destroyer, self).__init__(x, y, win, color)
+        self.drawTurret()
+
+    def drawTurret(self):
+        turret = Arc(Point(650,425), Point(725,375),0,180,"chord")
+        turret.draw(self.win)
