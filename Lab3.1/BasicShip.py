e1648a3cf0bba8d4bbced3ece50e2da6249892b6
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-13 11:04:24 -0500

    Update BasicShip.py

--- Lab3.1/BasicShip.py
+++ Lab3.1/BasicShip.py
@@ -9,35 +9,40 @@ class basicShip(object):
         self.color = color
         self.hit = False
         self.win = win
-        self.hull()
-        self.controlCenter()
-        self.windows()
+        self._hull()
+        self._controlCenter()
+        self._windows()
 
-    def hull(self):
+    def _hull(self):
         self.hull = Polygon(Point(self.x,self.y),Point((self.x + 75), (self.y + 50)), Point((self.x + 225 ), (self.y + 50)), Point((self.x + 300), (self.y)))
         self.hull.setFill(self.color)
         self.hull.draw(self.win)
 
-    def controlCenter(self):
+    def _controlCenter(self):
         self.controlCenter = Polygon(Point((self.x + 150 ), (self.y - 0)),Point((self.x + 175 ), (self.y - 75)),Point((self.x + 250 ), (self.y - 75)),Point((self.x + 250 ), (self.y - 0)))
         self.controlCenter.setFill(self.color)
         self.controlCenter.draw(self.win)
 
-    def windows(self):
+    def _windows(self):
+        self.windows = []
         addX = 165.5
         for i in range(0,4):
-            self.window = Rectangle(Point((self.x + addX),(self.y - 50)), Point((self.x + (addX + 12.5)),(self.y - 37.5)))
-            self.window.setFill(color_rgb(198,244,242))
-            self.window.draw(self.win)
-            addX += 15\
+            window = Rectangle(Point((self.x + addX),(self.y - 50)), Point((self.x + (addX + 12.5)),(self.y - 37.5)))
+            window.setFill(color_rgb(198,244,242))
+            window.draw(self.win)
+            self.windows.append(window)
+            addX += 15
 
     def move(self,x,y,win):
         self.x = x
         self.y = y
-        self.hull.undraw
+        self.hull.undraw()
         self.controlCenter.undraw()
-        self.windows.undraw()
+        for i in self.windows:
+            i.undraw()
+        self._hull()
+        self._controlCenter()
+        for i in self.windows:
+            i.draw(self.win)
 
 
-#ship = basicShip(600,350,win)
-#win.getMouse()
