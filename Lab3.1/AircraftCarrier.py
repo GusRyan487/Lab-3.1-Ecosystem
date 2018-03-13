2c050a0eae1511cb4bae61351be3cba0569be2df
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-13 11:03:24 -0500

    Update AircraftCarrier.py

--- Lab3.1/AircraftCarrier.py
+++ Lab3.1/AircraftCarrier.py
@@ -1,7 +1,15 @@
 from BasicShip import *
+import random
 class AircraftCarrier(basicShip):
     def __init__(self, x, y, win, color=(color_rgb(150, 155, 163))):
         super(AircraftCarrier,self).__init__(x,y,win,color)
+        self.acName = 'Aircraft Carrier #'
+        ran = random.randint(1, 100)
+        if ran < 10:
+            ran = '0' + str(ran)
+            self.acName += ran
 
-    def plane(self):
+
+    #def plane(self):
+        #self.body = Oval(Point())
 
