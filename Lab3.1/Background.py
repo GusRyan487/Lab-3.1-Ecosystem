43e4624f3964d9cfa50e07c504482ec97d7bab62
Skidaddle <36928558+MrGoose100@users.noreply.github.com>
2018-03-13 17:35:43 -0500

    Update Background.py

--- Lab3.1/Background.py
+++ Lab3.1/Background.py
@@ -1,7 +1,7 @@
 from graphics import *
 from BasicShip import *
-import time
-
+from Destroyer import *
+from AircraftCarrier import *
 
 win = GraphWin("Navy Fleet", 1000, 650, autoflush=False)
 
@@ -9,25 +9,31 @@ win = GraphWin("Navy Fleet", 1000, 650, autoflush=False)
 class Background(object):
 
     def __init__(self,win):
-        #self.land = land
+
         self.win = win
-        #self.house = house
+
         self.drawHouse()
         self.drawLand()
         self.drawOcean()
         self.drawSun()
+        self.shell = False
+
 
     def drawOcean(self):
+        """Drawing an ocean"""
         ocean = Rectangle(Point(0, 450), Point(999, 649))
         ocean.setFill(color_rgb(15, 144, 219))
         ocean.draw(self.win)
 
     def drawLand(self):
+        """Drawing land"""
         land = Rectangle(Point(0, 300), Point(300, 450))
         land.setFill(color_rgb(249, 235, 184))
         land.draw(self.win)
 
+
     def drawHouse(self):
+        """Drawing a housed with a door window on land"""
         house = Rectangle(Point(100, 200), Point(225, 300))
         house.setFill(color_rgb(73, 49, 5))
         house.draw(win)
@@ -42,14 +48,35 @@ class Background(object):
         window.draw(self.win)
 
     def drawSun(self):
+        """Drawing a sun"""
         sun = Circle(Point(999, 0), 100)
         sun.setFill("yellow")
         sun.draw(self.win)
+    def blast(self):
+        """shooting land with destroyer"""
+        if self.shell == True:
+            blast = Arc(Point(325, 300), Point(225, 450), 90, 180, "sector")
+            blast.setFill("white")
+            blast.draw(self.win)
+            blast.setOutline("white")
+            xtraBlast = Rectangle(Point(275,300),Point(300,450))
+            xtraBlast.setFill("white")
+            xtraBlast.draw(self.win)
+            xtraBlast.setOutline("white")
+        else:
+            print("Your island survives another day")
+
 
 
 win.setBackground(color_rgb(20, 200, 255))
 back = Background(win)
 ship = basicShip(625,400,win)
-ship.move(500,400,win)
+ac= AircraftCarrier(2,2,win)
+print(ac.acName)
+dest = Destroyer(2,2,win)
+print(dest.dName)
+
+
+
 win.getMouse()
 win.close()
