8e3102e51d09aad4844d7e61901d7c5e3463c7a1
Skidaddle <36928558+MrGoose100@users.noreply.github.com>
2018-03-13 11:04:27 -0500

    Add files via upload

--- Lab3.1/Destroyer.py
+++ Lab3.1/Destroyer.py
@@ -1,10 +1,20 @@
-
 from basicShip import *
+
 class Destroyer(basicShip):
     def __init__(self, x, y, win, color=(color_rgb(150, 155, 163))):
         super(Destroyer, self).__init__(x, y, win, color)
         self.drawTurret()
 
     def drawTurret(self):
-        turret = Arc(Point(650,425), Point(725,375),0,180,"chord")
+        gun = Rectangle(Point(650,387.5), Point(600,400))
+        gun.setFill(color_rgb(150,155,163))
+        gun.draw(self.win)
+        turret = Arc(Point(650, 425), Point(725, 375), 0, 180, "chord")
+        turret.setFill(color_rgb(150, 155, 163))
         turret.draw(self.win)
+
+
+
+
+
+
