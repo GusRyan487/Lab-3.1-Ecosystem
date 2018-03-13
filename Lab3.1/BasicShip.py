90dd1e94c86ed50d097e954886d6724674ee2f06
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-13 17:34:41 -0500

    Update BasicShip.py

--- Lab3.1/BasicShip.py
+++ Lab3.1/BasicShip.py
@@ -2,7 +2,6 @@ from graphics import *
 #win = GraphWin('Navy',1000,650,autoflush= False)
 class basicShip(object):
 
-
     def __init__(self,x,y,win,color = (color_rgb(150, 155, 163))):
         self.x = x
         self.y = y
@@ -12,28 +11,33 @@ class basicShip(object):
         self._hull()
         self._controlCenter()
         self._windows()
+        self.addX = 0
 
     def _hull(self):
+        '''Draws ship hull'''
         self.hull = Polygon(Point(self.x,self.y),Point((self.x + 75), (self.y + 50)), Point((self.x + 225 ), (self.y + 50)), Point((self.x + 300), (self.y)))
         self.hull.setFill(self.color)
         self.hull.draw(self.win)
 
     def _controlCenter(self):
+        '''Draws ship's control center'''
         self.controlCenter = Polygon(Point((self.x + 150 ), (self.y - 0)),Point((self.x + 175 ), (self.y - 75)),Point((self.x + 250 ), (self.y - 75)),Point((self.x + 250 ), (self.y - 0)))
         self.controlCenter.setFill(self.color)
         self.controlCenter.draw(self.win)
 
     def _windows(self):
+        '''Draws ship windows'''
         self.windows = []
-        addX = 165.5
+        self.addX = 165.5
         for i in range(0,4):
-            window = Rectangle(Point((self.x + addX),(self.y - 50)), Point((self.x + (addX + 12.5)),(self.y - 37.5)))
+            window = Rectangle(Point((self.x + self.addX),(self.y - 50)), Point((self.x + (self.addX + 12.5)),(self.y - 37.5)))
             window.setFill(color_rgb(198,244,242))
             window.draw(self.win)
             self.windows.append(window)
-            addX += 15
+            self.addX += 15
 
     def move(self,x,y,win):
+        '''Moves ship'''
         self.x = x
         self.y = y
         self.hull.undraw()
@@ -42,7 +46,7 @@ class basicShip(object):
             i.undraw()
         self._hull()
         self._controlCenter()
-        for i in self.windows:
-            i.draw(self.win)
+        self.addX = 165.5 + (self.x - x)
+        self._windows()
 
 
