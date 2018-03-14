23dc1bf5ce78cb0cb16b9e1afcf5b76fe306c9a0
Skidaddle <36928558+MrGoose100@users.noreply.github.com>
2018-03-14 11:03:54 -0500

    yes yes yes
    gud cod very yes yes yes very yes am gud progamer

--- Lab3.1/Destroyer.py
+++ Lab3.1/Destroyer.py
@@ -1,6 +1,6 @@
-from BasicShip import *
+from basicShip import *
 import random
-import time
+
 class Destroyer(basicShip):
     def __init__(self,x,y,win,color = (color_rgb(150, 155, 163))):
         super(Destroyer,self).__init__(x,y,win,color)
@@ -11,30 +11,23 @@ class Destroyer(basicShip):
         self.dName += str(ran)
         self.move = True
         self.drawTurret()
-        self.missle()
+        #self.missle()
 
 
     def drawTurret(self):
         """drawing an arc for a turret and a gun to shoot the land"""
-        gun = Rectangle(Point(650, 387.5), Point(600, 400))
+        gun = Rectangle(Point(self.x-50,self.y - 12.5), Point(self.x + 25,self.y + 0))
         gun.setFill(color_rgb(150, 155, 163))
         gun.draw(self.win)
         turret = Arc(Point(650, 425), Point(725, 375), 0, 180, "chord")
         turret.setFill(color_rgb(150, 155, 163))
         turret.draw(self.win)
 
-    def missle(self):
-        missle = Rectangle(Point(512.5,393.75), Point(537.5,400))
-        missle.setFill("black")
-        missle.draw(self.win)
-        while self.move == True:
-            if missle.getP1() == Point(300.0, 393.0):
-                missle.undraw()
-                self.move= False
-            else:
-                if self.move == True:
-                    #time.sleep(.5)
-                    missle.move(50,0)
+    #def missle(self):
+
+
+
+   
 
 
 
