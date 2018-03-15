0567ea2b6f0747068e913672c87abb685523e2e4
Skidaddle <36928558+MrGoose100@users.noreply.github.com>
2018-03-15 17:34:19 -0500

    ergreg
    ergerg

--- Lab3.1/Destroyer.py
+++ Lab3.1/Destroyer.py
@@ -5,27 +5,39 @@ class Destroyer(basicShip):
     def __init__(self,x,y,win,color = (color_rgb(150, 155, 163))):
         super(Destroyer,self).__init__(x,y,win,color)
         self.dName = "Destroyer #"
-        ran = random.randint(1, 100)
+        self.drawTurret()
+        self.createDName()
+        fired = True
+
+
+
+
+    def createDName(self):
+        ran = random.randint(1, 99)
         if ran < 10:
             ran = '0' + str(ran)
         self.dName += str(ran)
-        self.move = True
-        self.drawTurret()
-        #self.missle()
+        print("Created",self.dName)
+
 
 
     def drawTurret(self):
         """drawing an arc for a turret and a gun to shoot the land"""
-        gun = Rectangle(Point(self.x-50,self.y - 12.5), Point(self.x + 50,self.y - 5))
-        gun.setFill(color_rgb(150, 155, 163))
-        gun.draw(self.win)
-        turret = Arc(Point(self.x + 25, self.y+25), Point(self.x+100, self.y-25), 0, 180, "chord")
-        turret.setFill(color_rgb(150, 155, 163))
-        turret.draw(self.win)
+        self.gun = Rectangle(Point(self.x-50,self.y - 12.5), Point(self.x + 50,self.y - 5))
+        self.gun.setFill(color_rgb(150, 155, 163))
+        self.gun.draw(self.win)
+        self.turret = Arc(Point(self.x + 25, self.y+25), Point(self.x+100, self.y-25), 0, 180, "chord")
+        self.turret.setFill(color_rgb(150, 155, 163))
+        self.turret.draw(self.win)
         self.explosion = Image(Point(self.x - 100, self.y - 10), 'nope.png')
         self.explosion.draw(self.win)
 
-    #def missle(self):
+    def destMove(self, x, y):
+        self.move(x, y)
+        self.gun.undraw()
+        self.turret.undraw()
+        self.explosion.undraw()
+        self.drawTurret()
 
 
 
