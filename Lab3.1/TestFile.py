b0437c155d2c2108bd24cc783feb1979ce856364
Skidaddle <36928558+MrGoose100@users.noreply.github.com>
2018-03-15 17:35:36 -0500

    gus gay
    xd gottem jk - no no u's

---
+++ Lab3.1/TestFile.py
@@ -0,0 +1,22 @@
+from basicShip import *
+from Background import *
+from AircraftCarrier import *
+from Destroyer import *
+import time
+
+
+
+def main():
+    win.setBackground(color_rgb(20, 200, 255))
+    back = Background(win)
+    ac= AircraftCarrier(625,400,win)
+    dest = Destroyer(600,200,win)
+
+
+
+    dest.destMove(400,200)
+
+main()
+
+win.getMouse()
+win.close()
