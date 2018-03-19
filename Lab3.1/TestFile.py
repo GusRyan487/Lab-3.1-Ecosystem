b692e053c44d1862523f1dc88677b07e0da54b60
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-19 11:03:40 -0500

    Update TestFile.py

--- Lab3.1/TestFile.py
+++ Lab3.1/TestFile.py
@@ -1,4 +1,4 @@
-from basicShip import *
+from BasicShip import *
 from Background import *
 from AircraftCarrier import *
 from Destroyer import *
@@ -6,16 +6,34 @@ import time
 
 
 
+
+
 def main():
     '''runs the program'''
     win.setBackground(color_rgb(20, 200, 255))
     back = Background(win)
-    ac= AircraftCarrier(625,400,win)
-    dest = Destroyer(600,200,win)
+    dest = Destroyer(600, 400, win)
+    time.sleep(2)
+    x = 600
+    while x != 400:
+        dest.destMove(x,400)
+        x-= 5
+    dest.destMove(400, 400)
+    time.sleep(2)
+    back.blast()
+    dest.Turret.explosion.draw(win)
+    time.sleep(1.5)
+    x = 400
+    while x != 1000:
+        dest.destMove(x,400)
+        x+=5
 
+    ac = AircraftCarrier(625,400,win)
+    time.sleep(.5)
+    ac.acmove(600,400)
+    ac.Plane.dropTheBomb()
+    back.undrawHouse()
 
-
-    dest.destMove(400,200)
 
 main()
 
