aba45bed0dacac71ad9d2393053c26a4439f9d58
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-19 11:03:21 -0500

    Update Background.py

--- Lab3.1/Background.py
+++ Lab3.1/Background.py
@@ -3,7 +3,7 @@ from BasicShip import *
 from Destroyer import *
 from AircraftCarrier import *
 
-win = GraphWin("Navy Fleet", 1000, 650, autoflush=False)
+win = GraphWin("Navy Fleet", 1000, 650)
 
 
 class Background(object):
@@ -14,7 +14,6 @@ class Background(object):
         self.drawLand()
         self.drawOcean()
         self.drawSun()
-        self.shell = False
 
 
     def drawOcean(self):
@@ -32,18 +31,23 @@ class Background(object):
 
     def drawHouse(self):
         """Drawing a housed with a door window on land"""
-        house = Rectangle(Point(100, 200), Point(225, 300))
-        house.setFill(color_rgb(73, 49, 5))
-        house.draw(win)
-        roof = Polygon(Point(100, 200), Point(125, 175), Point(200, 175), Point(225, 200))
-        roof.setFill(color_rgb(40, 27, 2))
-        roof.draw(win)
-        door = Rectangle(Point(125, 300), Point(150, 250))
-        door.setFill(color_rgb(196, 135, 70))
-        door.draw(win)
-        window = Rectangle(Point(175, 250), Point(200, 225))
-        window.setFill(color_rgb(104, 222, 255))
-        window.draw(self.win)
+        self.house = Rectangle(Point(100, 200), Point(225, 300))
+        self.house.setFill(color_rgb(73, 49, 5))
+        self.house.draw(win)
+        self.roof = Polygon(Point(100, 200), Point(125, 175), Point(200, 175), Point(225, 200))
+        self.roof.setFill(color_rgb(40, 27, 2))
+        self.roof.draw(win)
+        self.door = Rectangle(Point(125, 300), Point(150, 250))
+        self.door.setFill(color_rgb(196, 135, 70))
+        self.door.draw(win)
+        self.window = Rectangle(Point(175, 250), Point(200, 225))
+        self.window.setFill(color_rgb(104, 222, 255))
+        self.window.draw(self.win)
+    def undrawHouse(self):
+        self.house.undraw()
+        self.roof.undraw()
+        self.door.undraw()
+        self.window.undraw()
 
     def drawSun(self):
         """Drawing a sun"""
@@ -52,26 +56,14 @@ class Background(object):
         sun.draw(self.win)
     def blast(self):
         """shooting land with destroyer"""
-        if self.shell == True:
-            blast = Arc(Point(325, 300), Point(225, 450), 90, 180, "sector")
-            blast.setFill("white")
-            blast.draw(self.win)
-            blast.setOutline("white")
-            xtraBlast = Rectangle(Point(275,300),Point(300,450))
-            xtraBlast.setFill("white")
-            xtraBlast.draw(self.win)
-            xtraBlast.setOutline("white")
+        blast = Arc(Point(325, 300), Point(225, 450), 90, 180, "sector")
+        blast.setFill(color_rgb(20, 200, 255))
+        blast.draw(self.win)
+        blast.setOutline(color_rgb(20, 200, 255))
+        xtraBlast = Rectangle(Point(275,300),Point(300,450))
+        xtraBlast.setFill(color_rgb(20, 200, 255))
+        xtraBlast.draw(self.win)
+        xtraBlast.setOutline(color_rgb(20, 200, 255))
 
 
 
-win.setBackground(color_rgb(20, 200, 255))
-back = Background(win)
-ac= AircraftCarrier(625,400,win)
-ac.acmove(500,400)
-ac.dropTheBomb()
-##################33aREMOVE HOUSE
-
-
-
-win.getMouse()
-win.close()
