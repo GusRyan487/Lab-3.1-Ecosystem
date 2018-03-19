fea6f96c5b3b435b81feb0bcd35d64991c9f591d
Gus Ryan <36928503+GusRyan487@users.noreply.github.com>
2018-03-19 11:03:03 -0500

    Update Destroyer.py

--- Lab3.1/Destroyer.py
+++ Lab3.1/Destroyer.py
@@ -1,16 +1,13 @@
-from basicShip import *
+from BasicShip import *
 import random
 
 class Destroyer(basicShip):
     def __init__(self,x,y,win,color = (color_rgb(150, 155, 163))):
         super(Destroyer,self).__init__(x,y,win,color)
         self.dName = "Destroyer #"
-        self.drawTurret()
+        self.Turret.drawTurret()
         self.createDName()
-        fired = True
-
-
-
+        self.Turret(self.win)
 
     def createDName(self):
         '''creates the destroyer id number'''
@@ -20,26 +17,31 @@ class Destroyer(basicShip):
         self.dName += str(ran)
         print("Created",self.dName)
 
-
-
-    def drawTurret(self):
-        """drawing an arc for a turret and a gun to shoot the land"""
-        self.gun = Rectangle(Point(self.x-50,self.y - 12.5), Point(self.x + 50,self.y - 5))
-        self.gun.setFill(color_rgb(150, 155, 163))
-        self.gun.draw(self.win)
-        self.turret = Arc(Point(self.x + 25, self.y+25), Point(self.x+100, self.y-25), 0, 180, "chord")
-        self.turret.setFill(color_rgb(150, 155, 163))
-        self.turret.draw(self.win)
-        self.explosion = Image(Point(self.x - 100, self.y - 10), 'nope.png')
-        self.explosion.draw(self.win)
-
     def destMove(self, x, y):
         '''moves gun and the destroyer'''
         self.move(x, y)
-        self.gun.undraw()
-        self.turret.undraw()
-        self.explosion.undraw()
-        self.drawTurret()
+        self.Turret.gun.undraw()
+        self.Turret.turret.undraw()
+        self.Turret.explosion.undraw()
+        self.Turret.drawTurret()
+
+    class Turret(object):
+        def __init__(self, win):
+            self.x =
+            self.gun = None
+            self.turret = None
+            self.explosion = None
+            self.win = win
+
+        def drawTurret(self):
+            """drawing an arc for a turret and a gun to shoot the land"""
+            self.gun = Rectangle(Point(self.x-50,self.y - 12.5), Point(self.x + 50,self.y - 5))
+            self.gun.setFill(color_rgb(150, 155, 163))
+            self.gun.draw(self.win)
+            self.turret = Arc(Point(self.x + 25, self.y+25), Point(self.x+100, self.y-25), 0, 180, "chord")
+            self.turret.setFill(color_rgb(150, 155, 163))
+            self.turret.draw(self.win)
+            self.explosion = Image(Point(self.x - 100, self.y - 10), 'nope.png')
 
 
 
